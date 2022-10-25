702c4ba0b2324deefe6ecd41384a3c7f02573335
chimbuchieke <113604794+chimbuchieke@users.noreply.github.com>
2022-10-25 17:05:34 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,11 +5,12 @@
  * @s: input string
  * Return: string in reverse
  */
-void rev_string (char *s)
+void rev_string(char *s)
 {
 	char rv = s[0];
 	int c = 0;
 	int i;
+	
 	while (s[c] != '\0')
 		c++;
 	for (i = 0; i < c; i++)
