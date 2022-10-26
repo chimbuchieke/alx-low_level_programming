0184dd96aba9025eefab89ce723e3bcfec9794c1
chimbuchieke <113604794+chimbuchieke@users.noreply.github.com>
2022-10-26 06:51:16 +0100

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,17 +9,16 @@ int main(void)
 {
 	int i;
 	
-	printf("infinite loop incomimg :(\n");
+	printf("Infinite loop incomimg :(\n");
 
 	i = 0;
-
+	
 	/*
-	*while (i < 10)
+	* while (i < 10)
 	* {
 	* putchar(i);
 	* }
 	*/
 	printf("infinite loop avoided! \\o/\n");
-
 	return (0);
 }
