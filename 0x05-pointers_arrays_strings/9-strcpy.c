9b9309f56d2e2835abc3a98037f1b76dd2919429
chimbuchieke <113604794+chimbuchieke@users.noreply.github.com>
2022-10-26 06:21:06 +0100

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,12 +2,11 @@
 #include <stdio.h>
 
 /**
- * _strcpy - copies the string pointed to by src
- * @dest: destination
- * @src: source
- * Return: string
- */
-
+* _strcpy - copies the string pointed to by src
+* @dest: destination
+* @src: source
+* Return: string
+*/
 char *_strcpy(char *dest, char *src)
 {
 	int count = 0;
